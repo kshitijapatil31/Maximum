910604233336205b085c385910cbf79d3d5a3d03
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-20 20:14:43 +0530

    usecase 3 solved

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -4,16 +4,17 @@ public class MaximumProblem {
 
 	public static void main(String[] args) {
 
-		Float x1=new Float(22.3);
-		Float x2=new Float(55.2);
-		Float x3=new Float (11.3);
+		String x1=new String("rohan");
+		String x2=new String("sachin");
+		String x3=new String("rohan");
+
 		
 		if(x1.compareTo(x2)>0&&x1.compareTo(x3)>0) {
-			System.out.println("maximum no is : "+x1);
+			System.out.println("maximum String is : "+x1);
 		}else if(x2.compareTo(x1)>0&&x2.compareTo(x3)>0) {
-			System.out.println("maximum no is : "+x2);
+			System.out.println("maximum String is : "+x2);
 		}else if(x3.compareTo(x1)>0&& x3.compareTo(x2)>0) {
-			System.out.println("maximum no is : "+x3);
+			System.out.println("maximum String is : "+x3);
 		}
 	}
 }
